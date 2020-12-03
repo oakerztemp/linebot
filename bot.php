4e6ee149f6be90c3fe426d12a1fd9afee5f139d0
oakerztemp <43953799+oakerztemp@users.noreply.github.com>
2020-12-03 14:54:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,8 @@ $url = "https://bitpay.com/api/rates";
 $json = json_decode(file_get_contents($url));
 $dollar = $btc = 0;
 foreach($json as $obj){
-   echo '1 bitcoin = $'. $obj->rate .' '. $obj->name .' ('. $obj->code .')<br>';
+   if ($obj->code = 'USD')
+      echo '1 bitcoin = $'. $obj->rate .' '. $obj->name .' ('. $obj->code .')<br>';
 }
 
 if ( sizeof($request_array['events']) > 0 ) {
